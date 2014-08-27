d1569a5998d0b4159f9d8bd388de17e0b0fef8d5
aarontesler01 <aht218@lehigh.edu>
2014-08-27 16:38:02 +0000

    Updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,30 @@
+/////////////////////////////////////////////////////
+//Aaron Tesler
+//Homework 01
+//WelcomeClass Java Program
+//
+//  First, create program
+//      See instructions on course site
+//  Second, compile program
+//      use javac
+//  Third, turn it in
+
+// What is so important about the next line?
+public class WelcomeClass{
+    
+    // And this line?
+    public static void main(String[] args) {
+        
+        // Time to make the box and message
+        System.out.println("    -----------     ");
+        System.out.println("    | WELCOME |     ");
+        System.out.println("    -----------     ");
+        System.out.println("    ^  ^  ^  ^  ^  ^");
+        System.out.println("   / \\/ \\/ \\/ \\/ \\/ \\");
+        System.out.println("  |-A--H--T--2--1--8-|");
+        System.out.println("   \\ /\\ /\\ /\\ /\\ /\\ /");
+        System.out.println("    v  v  v  v  v  v ");
+        System.out.println("I am now beginning my classes at Lehigh. Lots of new people.");
+        
+    }
+}
